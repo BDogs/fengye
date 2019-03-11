ea02e915b1a10919520d33aaa2f15da96bf38e70
BDogs <1215852067@qq.com>
2019-03-11 18:04:40 +0800

    uwsgi阿里云运行环境搭建

--- fengye/settings.py
+++ fengye/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'fengye.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fengye20190108',
+        'USER': 'root',
+        'PASSWORD': 'Song12138',
+        'HOST': 'localhost',
+        'PORT': '3306',
+}
 }
 
 
